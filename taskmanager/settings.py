fc58db469779ff32f093a7c9ab7f70264f4cbdc0
Ali Mashal <a.mashalwork786@gmail.com>
2022-04-24 08:17:07 +0430

    db connect

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = "taskmanager.wsgi.application"
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'project_management',
-        'USER': 'project_management',
-        'PASSWORD': 'project_management',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
         'POST': '',
