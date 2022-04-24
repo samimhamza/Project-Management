8b57c50476b4f2c8e8ae4780f792465b3c7ba096
samimhamza <samim.hamza7@gmail.com>
2022-04-24 10:47:15 +0430

    new changes added

--- users/models.py
+++ users/models.py
@@ -9,20 +9,29 @@ class Team(models.Model):
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 
 class TeamUser(models.Model):
     team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.type
 
 
 class UserNote(models.Model):
@@ -31,7 +40,10 @@ class UserNote(models.Model):
     color = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name + " " + self.color
 
 
 class Reminder(models.Model):
@@ -40,3 +52,6 @@ class Reminder(models.Model):
     remind_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name + " " + self.remind_at
