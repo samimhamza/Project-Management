407b202864f730a1e9cf167228ad0a51b6167cef
ghafoorfahim81 <82256682+ghafoorfahim81@users.noreply.github.com>
2022-04-24 12:56:54 +0430

    some changes

--- projects/models.py
+++ projects/models.py
@@ -16,7 +16,7 @@ class Project(models.Model):
     project_details     = models.JSONField()
     company_name        = models.CharField(max_length=100)
     company_location    = models.CharField(max_length=100)
-    created_at          = models.DateTimeField()
-    updated_at          = models.DateTimeField()
+    created_at          = models.DateTimeField(auto_now_add=True)
+    updated_at          = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
