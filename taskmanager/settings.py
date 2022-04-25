290821404c574e5b7fafcf3ec38fb80f5ffa775c
ghafoorfahim81 <82256682+ghafoorfahim81@users.noreply.github.com>
2022-04-25 08:32:19 +0430

    afg

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
-    "polymorphic",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
