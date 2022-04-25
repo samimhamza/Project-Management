816f8e2f1988552e7d4bb46d93f292fa5d894164
samimhamza <samim.hamza7@gmail.com>
2022-04-25 12:03:17 +0430

    create read update and delete api completed on project table

--- projects/api/urls.py
+++ projects/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from projects.api.views import ProjectListCreateAPIView
+from projects.api.views import ProjectListCreateAPIView, ProjectDetailAPIView
 
 
 urlpatterns = [
-    path("projects/", ProjectListCreateAPIView.as_view(), name="projects-list")
+    path("projects/", ProjectListCreateAPIView.as_view(), name="projects-list"),
+    path("projects/<uuid:pk>", ProjectDetailAPIView.as_view(), name="projects-detail"),
 ]
--- projects/api/views.py
+++ projects/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from projects.models import Project
 from projects.api.serializers import ProjectSerializer
 from rest_framework.response import Response
-
+from rest_framework.generics import get_object_or_404
 
 # class ProjectListCreateAPIView(
 #     mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
@@ -30,3 +30,27 @@ class ProjectListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProjectDetailAPIView(APIView):
+    def get_object(self, pk):
+        project = get_object_or_404(Project, pk=pk)
+        return project
+
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+      project = self.get_object(pk)
+      project.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
