8a3ffd50460f402b5d47701e7f471a7954297e7e
samimhamza <samim.hamza7@gmail.com>
2022-04-26 09:03:17 +0430

    tasks api completed

--- tasks/api/views.py
+++ tasks/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from tasks.api.serializers import TaskSerializer, UserTaskSerializer, CommentSerializer
-from tasks.models import Task, UserTask
+from tasks.models import Task, UserTask, Comment
 import datetime
 
 # Task CRUD
@@ -92,3 +92,49 @@ class UserTaskDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
 
 # end of UserTask CRUD
+
+
+# Comment CRUD
+class CommentListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    paginate_by = 10
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            if not request.data._mutable:
+                request.data._mutable = True
+                request.data.update(created_by=request.user.id)
+                request.data.update(updated_by=request.user.id)
+        except:
+            request.data.update(created_by=request.user.id)
+            request.data.update(updated_by=request.user.id)
+        return self.create(request, *args, **kwargs)
+
+
+class CommentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        try:
+            if not request.data._mutable:
+                request.data._mutable = True
+                request.data.update(updated_by=request.user.id)
+                request.data.update(updated_at=datetime.datetime.now())
+        except:
+            request.data.update(updated_by=request.user.id)
+            request.data.update(updated_at=datetime.datetime.now())
+        return self.update(request, *args, **kwargs)
+
+
+# end of Comment CRUD
