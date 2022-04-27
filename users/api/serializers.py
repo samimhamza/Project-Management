c994609310ec253a379e767255ea1c518aa1e788
samimhamza <samim.hamza7@gmail.com>
2022-04-27 10:22:40 +0430

    s

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -5,7 +5,7 @@ from users.models import User, Team, UserNote, Reminder, Holiday, Notification, 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "password", "first_name", "last_name", "email"]
+        fields = "__all__"
 
 
 class TeamUserSerializer(serializers.ModelSerializer):
