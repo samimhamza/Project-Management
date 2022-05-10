316a89ed1a6c8657c74d6d32366f2bd9cee7de76
ghafoorfahim81 <82256682+ghafoorfahim81@users.noreply.github.com>
2022-05-10 10:01:59 +0430

    asfasdf

--- projects/api/urls.py
+++ projects/api/urls.py
@@ -4,5 +4,6 @@ from projects.api.views import ProjectListCreateAPIView, ProjectDetailAPIView
 
 urlpatterns = [
     path("projects/", ProjectListCreateAPIView.as_view(), name="projects-list"),
+    path("projects/create", ProjectListCreateAPIView.as_view(), name="projects-create"),
     path("projects/<uuid:pk>", ProjectDetailAPIView.as_view(), name="projects-detail"),
 ]
