4bd17a12bc86c46704b5cc213d1157f294626431
samimhamza <samim.hamza7@gmail.com>
2022-05-17 14:00:37 +0430

    project serializers done

--- projects/api/serializers.py
+++ projects/api/serializers.py
@@ -1,28 +1,53 @@
 from rest_framework import serializers
 from projects.models import Project, Country, Location, FocalPoint, Income, Payment
-from users.api.serializers import UserSerializer
+from users.models import User, Team
 
 
-class LocationSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Location
-        fields = "__all__"
+        model = User
+        fields = ["first_name", "last_name", "email"]
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    company_location = LocationSerializer(read_only=True)
-    users = UserSerializer(many=True, read_only=True)
-    teams = UserSerializer(many=True, read_only=True)
-
+class TeamSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Project
-        fields = "__all__"
+        model = Team
+        fields = ["name", "description"]
 
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = "__all__"
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    country = CountrySerializer(read_only=True)
+
+    class Meta:
+        model = Location
+        fields = "__all__"
+
+
+class CustomLocationSerializer(serializers.ModelSerializer):
+    country = CountrySerializer(read_only=True)
+
+    class Meta:
+        model = Location
+        fields = ["address_line_one", "address_line_two", "city", "state", "country"]
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    company_location = CustomLocationSerializer(read_only=True)
+    users = UserSerializer(many=True, read_only=True)
+    teams = TeamSerializer(many=True, read_only=True)
+    created_by = UserSerializer(read_only=True)
+    updated_by = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Project
+        fields = "__all__"
+        depth = 2
 
 
 class FocalPointSerializer(serializers.ModelSerializer):
