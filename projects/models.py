77ae04e3471eb7cf5c46410fc7306cf2df96a756
samimhamza <samim.hamza7@gmail.com>
2022-05-18 10:03:31 +0430

    changes

--- projects/models.py
+++ projects/models.py
@@ -12,8 +12,8 @@ class Attachment(models.Model):
 
     # Below the mandatory fields for generic relation
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
+    object_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    content_object = GenericForeignKey("content_type", "object_id")
 
     def __str__(self):
         return self.name
@@ -24,7 +24,7 @@ class Reason(models.Model):
 
     # Below the mandatory fields for generic relation
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
+    object_id = models.UUIDField(default=uuid.uuid4, editable=False)
     content_object = GenericForeignKey()
 
 
@@ -103,8 +103,12 @@ class Project(models.Model):
         null=True,
         related_name="project_updated_by",
     )
-    attachments = GenericRelation(Attachment)
-    reasons = GenericRelation(Reason)
+    attachments = GenericRelation(
+        Attachment, content_type_field="content_type", object_id_field="object_id"
+    )
+    reasons = GenericRelation(
+        Reason, content_type_field="content_type", object_id_field="object_id"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
