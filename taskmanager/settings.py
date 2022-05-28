6716567f17c463ce283fdd05bfd5a5ff75118e2b
Ali Mashal <a.mashalwork786@gmail.com>
2022-05-28 08:33:22 +0430

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
-    'rest_framework_simplejwt.token_blacklist',
+    # 'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
     "projects",
     "expenses",
