1cfedc30fcf43a28ec657a0b806ecfa1b9ef64a7
samimhamza <samim.hamza7@gmail.com>
2022-05-29 14:40:17 +0430

    new changes

--- expenses/api/serializers.py
+++ expenses/api/serializers.py
@@ -18,7 +18,8 @@ class ExpenseSerializer(serializers.ModelSerializer):
     items = serializers.SerializerMethodField()
 
     def get_items(self, expense):
-        qs = ExpenseItem.objects.filter(deleted_at__isnull=True, expense=expense)
+        qs = ExpenseItem.objects.filter(
+            deleted_at__isnull=True, expense=expense)
         serializer = ExpenseItemSerializer(instance=qs, many=True)
         return serializer.data
 
@@ -31,9 +32,3 @@ class LessFieldExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
         fields = ["id", "title", "data"]
-
-
-class CreateExpenseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Expense
-        fields = ["title", "category", "date", "project"]
