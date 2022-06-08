470c374b867ea7ca74467a2eccab445f9a1c70b7
samimhamza <samim.hamza7@gmail.com>
2022-06-08 10:52:25 +0430

    changes

--- projects/api/views.py
+++ projects/api/views.py
@@ -97,6 +97,7 @@ class FocalPointViewSet(viewsets.ModelViewSet):
     queryset = FocalPoint.objects.all()
     serializer_class = FocalPointSerializer
     permission_classes = (FocalPointPermissions,)
+
     def list(self, request):
         queryset = self.get_queryset()
         if request.GET.get("project_id"):
@@ -104,15 +105,5 @@ class FocalPointViewSet(viewsets.ModelViewSet):
                 deleted_at__isnull=True, project=request.GET.get("project_id")).order_by("-created_at")
             serializer = self.get_serializer(queryset, many=True)
             return Response(serializer.data)
-
-        # pusher_client = pusher.Pusher(
-        #     app_id='1419045',
-        #     key='237907cedac4eed704cd',
-        #     secret='2afd20009cf5404d0df6',
-        #     cluster='ap2',
-        #     ssl=True
-        # )
-        # pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world Django'})
-
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
