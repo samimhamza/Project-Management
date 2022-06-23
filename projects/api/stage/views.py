4c72821c1d7a26a673970eea1ec9e696b3bd0fa0
samimhamza <samim.hamza7@gmail.com>
2022-06-23 08:11:28 +0430

    changes

--- projects/api/stage/views.py
+++ projects/api/stage/views.py
@@ -1,7 +1,8 @@
 from .serializers import StageSerializer, StageListSerializer, SubStageSerializer, SubStageListSerializer
+from common.actions import allItems, filterRecords, delete, trashList, withTrashed, restore
 from common.permissions_scopes import StagePermissions, SubStagePermissions
 from common.custom import CustomPageNumberPagination
-from common.actions import allItems, filterRecords
+from rest_framework.decorators import action
 from projects.models import Stage, SubStage
 from rest_framework import viewsets
 
@@ -22,6 +23,22 @@ class StageViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(page, many=True)
         return self.get_paginated_response(serializer.data)
 
+    def destroy(self, request, pk=None):
+        return delete(self, request, Stage)
+
+    @action(detail=False, methods=["get"])
+    def all(self, request):
+        return withTrashed(self, Stage, order_by="-created_at")
+
+    @action(detail=False, methods=["get"])
+    def trashed(self, request):
+        return trashList(self, Stage)
+
+    # for multi and single restore
+    @action(detail=False, methods=["get"])
+    def restore(self, request, pk=None):
+        return restore(self, request, Stage)
+
 
 class SubStageViewSet(viewsets.ModelViewSet):
     queryset = SubStage.objects.all()
@@ -38,3 +55,19 @@ class SubStageViewSet(viewsets.ModelViewSet):
         page = self.paginate_queryset(queryset)
         serializer = self.get_serializer(page, many=True)
         return self.get_paginated_response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        return delete(self, request, SubStage)
+
+    @action(detail=False, methods=["get"])
+    def all(self, request):
+        return withTrashed(self, SubStage, order_by="-created_at")
+
+    @action(detail=False, methods=["get"])
+    def trashed(self, request):
+        return trashList(self, SubStage)
+
+    # for multi and single restore
+    @action(detail=False, methods=["get"])
+    def restore(self, request, pk=None):
+        return restore(self, request, SubStage)
