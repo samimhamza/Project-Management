5b820011949ce40aefaa461fec35abb32688c778
Samim <samim.hamza7@gmail.com>
2022-07-05 14:55:46 +0430

    changes

--- clients/models.py
+++ clients/models.py
@@ -284,7 +284,9 @@ class ClientFeature(models.Model):
 
     def __str__(self):
         if self.client:
-            return self.client.first_name + " " + (self.feature.name if self.feature else "") + " " + self.plan if self.plan else ""
+            return self.client.first_name + " " \
+                + (self.feature.name if self.feature else "") + \
+                " " + self.plan if self.plan else ""
         else:
             return "No Client"
 
