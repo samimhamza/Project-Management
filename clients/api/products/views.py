872193e46ef2b5a3313eff3727068fecd30e4b86
Samim Hamza <samim.hamza7@gmail.com>
2022-07-16 11:06:31 +0430

    product CRUD Done

--- clients/api/products/views.py
+++ clients/api/products/views.py
@@ -1,11 +1,12 @@
-from multiprocessing import context
+from common.actions import (filterRecords, allItems, withTrashed,
+                            trashList, restore, delete, convertBase64ToImage)
 from .serializers import ProductSerializer, ProductListSerializer
 from common.custom import CustomPageNumberPagination
-from common.actions import filterRecords, allItems
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from clients.models import Product
+import os
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -13,6 +14,9 @@ class ProductViewSet(viewsets.ModelViewSet):
         deleted_at__isnull=True).order_by("-created_at")
     serializer_class = ProductSerializer
     pagination_class = CustomPageNumberPagination
+    queryset_actions = {
+        "destroy": Product.objects.all(),
+    }
 
     def list(self, request):
         queryset = self.get_queryset()
@@ -28,3 +32,56 @@ class ProductViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(
             page, many=True, context={"request": request})
         return self.get_paginated_response(serializer.data)
+
+    def create(self, request):
+        data = request.data
+        imageField = convertBase64ToImage(data["photo"])
+        creator = request.user
+        new_product = Product.objects.create(
+            photo=imageField,
+            name=data['name'],
+            details=data["details"],
+            developed_by=data["developed_by"],
+            created_by=creator,
+            updated_by=creator,
+        )
+        new_product.save()
+        serializer = self.get_serializer(
+            new_product, context={"request": request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        product = self.get_object()
+        if "photo" in request.data:
+            imageField = convertBase64ToImage(request.data["photo"])
+            product.photo = imageField
+            if os.path.isfile('media/'+str(product.photo)):
+                os.remove('media/'+str(product.photo))
+        for key, value in request.data.items():
+            if key != "photo":
+                setattr(product, key, value)
+        product.updated_by = request.user
+        product.save()
+        serializer = self.get_serializer(product, context={"request": request})
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):
+        return delete(self, request, Product)
+
+    @action(detail=False, methods=["get"])
+    def all(self, request):
+        return withTrashed(self, Product, order_by="-created_at")
+
+    @action(detail=False, methods=["get"])
+    def trashed(self, request):
+        return trashList(self, Product)
+
+    @action(detail=False, methods=["put"])
+    def restore(self, request, pk=None):
+        return restore(self, request, Product)
+
+    def get_queryset(self):
+        try:
+            return self.queryset_actions[self.action]
+        except (KeyError, AttributeError):
+            return super().get_queryset()
