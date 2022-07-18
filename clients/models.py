9d1d0d07e2cc9f982104cadc2a831996bbe2de79
Samim Hamza <samim.hamza7@gmail.com>
2022-07-18 15:50:41 +0430

    changes

--- clients/models.py
+++ clients/models.py
@@ -223,6 +223,7 @@ class PricePlan(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     plan_name = models.CharField(max_length=128)
     plan_price = models.FloatField(max_length=120, null=True, blank=True)
+    duration = models.IntegerField()
     feature = models.ForeignKey(
         Feature, null=True, on_delete=models.CASCADE, related_name="price_plans")
 
