66a5cda8f3f604c872dc9d7155de4c3f8e7a042f
Samim Hamza <samim.hamza7@gmail.com>
2022-07-27 14:04:08 +0430

    roles export done

--- users/api/views.py
+++ users/api/views.py
@@ -107,6 +107,8 @@ class RoleViewSet(Repository):
         queryset = filterRecords(queryset, request,  table=Role)
         if request.GET.get("items_per_page") == "-1":
             return allItems(RoleListSerializer, queryset)
+        if request.GET.get("items_per_page") == "-2":
+            return allItems(self.get_serializer, queryset)
 
         page = self.paginate_queryset(queryset)
         serializer = self.get_serializer(
