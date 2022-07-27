35b59dbc39d40b6481a397472a450a8d6f17d189
Samim Hamza <samim.hamza7@gmail.com>
2022-07-27 14:36:33 +0430

    services export done

--- clients/api/services/views.py
+++ clients/api/services/views.py
@@ -23,6 +23,8 @@ class ServiceViewSet(Repository):
         queryset = filterRecords(queryset, request, table=Service)
         if request.GET.get("items_per_page") == "-1":
             return allItems(ServiceListSerializer, queryset)
+        if request.GET.get("items_per_page") == "-2":
+            return allItems(self.get_serializer, queryset)
 
         page = self.paginate_queryset(queryset)
         serializer = self.get_serializer(page, many=True)
