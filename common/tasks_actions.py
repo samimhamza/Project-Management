66e3acfa26db68d482820b4e77b8814276fafd53
Samim Hamza <samim.hamza7@gmail.com>
2022-08-02 09:25:55 +0430

    changes

--- common/tasks_actions.py
+++ common/tasks_actions.py
@@ -162,7 +162,7 @@ def tasksOfProject(self, request, queryset):
     if request.GET.get("items_per_page") == "-1":
         if request.GET.get("extract_stages"):
             queryset = queryset.exclude(
-                type="stage", childs__type="sub_stage")
+                type="stage", childs__type="sub_stage").order_by("type")
             return allItems(LessFieldsTaskSerializer, queryset)
         return allItems(LessFieldsTaskSerializer, queryset)
     if request.GET.get("items_per_page") == "-2":
