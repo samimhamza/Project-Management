16510898175b3ddf66d566b47f85cf223a46a9ec
Mohammad Naweed <moh.naweed1@gmail.com>
2022-08-14 16:13:36 +0430

    dashboard top boxed finished

--- taskmanager/api/views.py
+++ taskmanager/api/views.py
@@ -1,8 +1,58 @@
 from .serializers import MyTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from projects.models import Project, Income
+from tasks.models import Task
+from users.models import User, Team
+from clients.models import Client
+from expenses.models import ExpenseItem
+
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = [AllowAny]
     serializer_class = MyTokenObtainPairSerializer
+
+@api_view()
+def counterTables(request):
+    result = []
+    if request.query_params.get('project_id'):  
+        user_ids = Project.objects.filter(pk = request.GET['project_id']).values_list('users')
+        team_ids = Project.objects.filter(pk = request.GET['project_id']).values_list('teams')
+        incomes = Income.objects.filter(deleted_at__isnull=True, project = request.GET['project_id'])
+        expense = ExpenseItem.objects.filter(deleted_at__isnull=True, expense__project=request.GET['project_id'])
+        result.append(counter(Task.objects.filter(project = request.GET['project_id']),"Tasks","fi fi-rr-list-check"))
+        result.append(counter(Team.objects.filter(deleted_at__isnull=True,pk__in=team_ids),"Teams","fi fi-rr-users-alt"))
+        result.append(counter(User.objects.filter(deleted_at__isnull=True,pk__in=user_ids),"Users","fi fi-rr-users"))
+    else:
+        result.append(counter(Project.objects.filter(deleted_at__isnull=True),"Projects","fi fi-rr-document"))
+        result.append(counter(Team.objects.filter(deleted_at__isnull=True),"Teams","fi fi-rr-users-alt"))
+        result.append(counter(User.objects.filter(deleted_at__isnull=True),"Users","fi fi-rr-users"))
+        result.append(counter(Client.objects.filter(deleted_at__isnull=True),"Clients","fi fi-rr-portrait"))
+        incomes = Income.objects.filter(deleted_at__isnull=True)
+        expense = ExpenseItem.objects.filter(deleted_at__isnull=True)
+
+    result.insert(1,{"name":"expenses","total": totalExpense(expense), "icon": "fi fi-rr-money-bill-wave"})
+    result.insert(1,{"name":"incomes","total": totalIncome(incomes), "icon":"fi fi-rr-sack-dollar"})
+    return Response(result)
+    
+
+
+def counter(model, name, icon):
+    result = {"name": name, "total":model.count(), "icon":icon}
+    return result
+
+def totalIncome(incomes):
+    total = 0
+    for income in incomes:
+        total += income.amount
+    return total
+
+def totalExpense(expenses):
+    total = 0
+    for expense in expenses:
+        total += expense.quantity * expense.cost
+    return total
+    
--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -6,20 +6,23 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
+
 )
-from taskmanager.api.views import MyObtainTokenPairView
+from taskmanager.api.views import MyObtainTokenPairView, counterTables
 
 urlpatterns = [
     # path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/", MyObtainTokenPairView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view()),
     path("api/token/verify/", TokenVerifyView.as_view()),
+    path("api/counter/", counterTables),
     path("admin/", admin.site.urls),
     path("api/", include("projects.api.urls")),
     path("api/", include("tasks.api.urls")),
     path("api/", include("users.api.urls")),
     path("api/", include("expenses.api.urls")),
     path("api/", include("clients.api.urls")),
+
 
 ] + static(settings.MEDIA_URL,
            document_root=settings.MEDIA_ROOT)
