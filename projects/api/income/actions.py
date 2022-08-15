63ee3f3c908868da68b8592ba6f066cbc9bec99e
Samim Hamza <samim.hamza7@gmail.com>
2022-08-15 12:12:45 +0430

    changes

---
+++ projects/api/income/actions.py
@@ -0,0 +1,81 @@
+from common.actions import getAttachments, checkProjectScope, unAuthorized
+from rest_framework.response import Response
+from projects.models import Income, Payment
+from rest_framework import status
+
+
+def checkAndReturn(user, project, scope, method):
+    if checkProjectScope(user, project, scope):
+        return method
+    else:
+        return unAuthorized()
+
+
+def incomeList(self, request, queryset, project):
+    queryset = queryset.filter(project=request.GET.get(
+        "project_id")).order_by("-created_at")
+    page = self.paginate_queryset(queryset)
+    serializer = self.get_serializer(
+        page, many=True, context={"request": request})
+    for data in serializer.data:
+        data = getAttachments(
+            request, data, data['id'], 'income_attachments_v', project)
+    return self.get_paginated_response(serializer.data)
+
+
+def incomeRetrieve(self, request, income):
+    serializer = self.get_serializer(income, context={"request": request})
+    data = serializer.data
+    data = getAttachments(
+        request, data, data['id'], 'income_attachments_v')
+    return Response(data)
+
+
+def incomeCreate(self, request, data, project):
+    income = Income.objects.create(
+        title=data["title"],
+        type=data["type"],
+        amount=data["amount"],
+        date=data["date"],
+        project=project,
+        created_by=data["created_by"],
+        updated_by=data["created_by"],
+    )
+    income.save()
+    serializer = self.get_serializer(
+        income, context={"request": request})
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+def incomeUpdate(self, request, income):
+    for key, value in request.data.items():
+        setattr(income, key, value)
+    income.updated_by = request.user
+    income.save()
+    serializer = self.get_serializer(income, context={"request": request})
+    return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+
+def paymentCreate(self, data, income):
+    payment = Payment.objects.create(
+        source=data["source"],
+        amount=data["amount"],
+        date=data["date"],
+        payment_method=data["payment_method"],
+        income=income,
+        created_by=data["created_by"],
+        updated_by=data["created_by"],
+    )
+    payment.save()
+    serializer = self.get_serializer(
+        payment)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+def paymentUpdate(self, request, payment):
+    for key, value in request.data.items():
+        setattr(payment, key, value)
+    payment.updated_by = request.user
+    payment.save()
+    serializer = self.get_serializer(payment, context={"request": request})
+    return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
--- projects/api/income/views.py
+++ projects/api/income/views.py
@@ -1,5 +1,10 @@
-from common.actions import filterRecords, addAttachment, deleteAttachments, getAttachments
+from .actions import (checkAndReturn, incomeCreate, paymentUpdate,
+                      incomeList, incomeRetrieve, incomeUpdate, paymentCreate)
+from common.actions import (filterRecords, addAttachment, deleteAttachments,
+                            checkProjectScope, unAuthorized, delete)
 from common.permissions_scopes import IncomePermissions, PaymentPermissions
+from rest_framework.permissions import IsAuthenticated
+from common.custom import CustomPageNumberPagination
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from common.Repository import Repository
@@ -9,52 +14,12 @@ from projects.api.serializers import (
     IncomeTrashedSerializer,
     PaymentTrashedSerializer
 )
-from rest_framework import status
+from rest_framework import status, viewsets
 from projects.models import (
     Income,
     Payment,
     Project
 )
-
-
-class PaymentViewSet(Repository):
-    model = Payment
-    queryset = Payment.objects.filter(deleted_at__isnull=True)
-    serializer_class = PaymentSerializer
-    permission_classes = (PaymentPermissions,)
-    serializer_action_classes = {
-        "trashed": PaymentTrashedSerializer
-    }
-
-    def create(self, request):
-        data = request.data
-        data["created_by"] = request.user
-        try:
-            income = Income.objects.get(pk=data["income"])
-        except Income.DoesNotExist:
-            return Response({"error": "Income does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-        income = Payment.objects.create(
-            source=data["source"],
-            amount=data["amount"],
-            date=data["date"],
-            payment_method=data["payment_method"],
-            income=income,
-            created_by=data["created_by"],
-            updated_by=data["created_by"],
-        )
-        income.save()
-        serializer = self.get_serializer(
-            income)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def update(self, request, pk=None):
-        payment = self.get_object()
-        for key, value in request.data.items():
-            setattr(payment, key, value)
-        payment.updated_by = request.user
-        payment.save()
-        serializer = self.get_serializer(payment, context={"request": request})
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
 
 class IncomeViewSet(Repository):
@@ -71,15 +36,7 @@ class IncomeViewSet(Repository):
         queryset = self.get_queryset()
         queryset = filterRecords(queryset, request, table=Income)
         if request.GET.get("project_id"):
-            queryset = queryset.filter(project=request.GET.get(
-                "project_id")).order_by("-created_at")
-            page = self.paginate_queryset(queryset)
-            serializer = self.get_serializer(
-                page, many=True, context={"request": request})
-            for data in serializer.data:
-                data = getAttachments(
-                    request, data, data['id'], 'income_attachments_v')
-            return self.get_paginated_response(serializer.data)
+            return incomeList(self, request, queryset)
         page = self.paginate_queryset(queryset)
         serializer = self.get_serializer(
             page, many=True, context={"request": request})
@@ -87,11 +44,7 @@ class IncomeViewSet(Repository):
 
     def retrieve(self, request, pk=None):
         income = self.get_object()
-        serializer = self.get_serializer(income, context={"request": request})
-        data = serializer.data
-        data = getAttachments(
-            request, data, data['id'], 'income_attachments_v')
-        return Response(data)
+        return incomeRetrieve(self, request, income)
 
     def create(self, request):
         data = request.data
@@ -100,19 +53,7 @@ class IncomeViewSet(Repository):
             project = Project.objects.get(pk=data["project"])
         except Project.DoesNotExist:
             return Response({"error": "Project does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-        income = Income.objects.create(
-            title=data["title"],
-            type=data["type"],
-            amount=data["amount"],
-            date=data["date"],
-            project=project,
-            created_by=data["created_by"],
-            updated_by=data["created_by"],
-        )
-        income.save()
-        serializer = self.get_serializer(
-            income, context={"request": request})
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return incomeCreate(self, request, data, project)
 
     def update(self, request, pk=None):
         income = self.get_object()
@@ -136,3 +77,154 @@ class IncomeViewSet(Repository):
     @action(detail=True, methods=["delete"])
     def delete_attachments(self, request, pk=None):
         return deleteAttachments(self, request)
+
+
+class PaymentViewSet(Repository):
+    model = Payment
+    queryset = Payment.objects.filter(deleted_at__isnull=True)
+    serializer_class = PaymentSerializer
+    permission_classes = (PaymentPermissions,)
+    serializer_action_classes = {
+        "trashed": PaymentTrashedSerializer
+    }
+
+    def create(self, request):
+        data = request.data
+        data["created_by"] = request.user
+        try:
+            income = Income.objects.get(pk=data["income"])
+        except Income.DoesNotExist:
+            return Response({"error": "Income does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+        return paymentCreate(self, data, income)
+
+    def update(self, request, pk=None):
+        payment = self.get_object()
+        return paymentUpdate(self, request, payment)
+
+
+class MyIncomeViewSet(viewsets.ModelViewSet):
+    queryset = Income.objects.filter(
+        deleted_at__isnull=True).order_by("-created_at")
+    pagination_class = CustomPageNumberPagination
+    serializer_class = IncomeSerializer
+    permission_classes = (IsAuthenticated,)
+    serializer_action_classes = {
+        "trashed": IncomeTrashedSerializer
+    }
+
+    def list(self, request):
+        if request.GET.get("project_id"):
+            try:
+                project = Project.objects.get(pk=request.GET.get("project_id"))
+            except Project.DoesNotExist:
+                return unAuthorized()
+            if checkProjectScope(request.user, project, "project_incomes_v"):
+                queryset = self.get_queryset()
+                queryset = filterRecords(queryset, request, table=Income)
+                return incomeList(self, request, queryset, project)
+            else:
+                return unAuthorized()
+        else:
+            return unAuthorized()
+
+    def retrieve(self, request, pk=None):
+        income = self.get_object()
+        return checkAndReturn(request.user, income.project, "project_incomes_v",
+                              incomeRetrieve(self, request, income))
+
+    def create(self, request):
+        data = request.data
+        data["created_by"] = request.user
+        try:
+            project = Project.objects.get(pk=data["project"])
+        except Project.DoesNotExist:
+            return Response({"error": "Project does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+        return checkAndReturn(request.user, project, "project_incomes_c",
+                              incomeCreate(self, request, data, project))
+
+    def update(self, request, pk=None):
+        income = self.get_object()
+        return checkAndReturn(request.user, income.project, "project_incomes_u",
+                              incomeUpdate(self, request, income))
+
+    @action(detail=True, methods=["post"])
+    def add_attachments(self, request, pk=None):
+        try:
+            income = self.get_object()
+            return checkAndReturn(request.user, income.project, "income_attachments_c",
+                                  addAttachment(request, income))
+        except:
+            return Response(
+                {"message": "something went wrong"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+    @action(detail=True, methods=["delete"])
+    def delete_attachments(self, request, pk=None):
+        income = self.get_object()
+        return checkAndReturn(request.user, income.project, "income_attachments_d",
+                              deleteAttachments(self, request))
+
+    def destroy(self, request, pk=None):
+        income = self.get_object()
+        return checkAndReturn(request.user, income.project, "project_incomes_d",
+                              delete(self, request, Income))
+
+    def get_serializer_class(self):
+        try:
+            return self.serializer_action_classes[self.action]
+        except(KeyError, AttributeError):
+            return super().get_serializer_class()
+
+    def get_queryset(self):
+        try:
+            return self.queryset_actions[self.action]
+        except (KeyError, AttributeError):
+            return super().get_queryset()
+
+
+class MyPaymentViewSet(viewsets.ModelViewSet):
+    model = Payment
+    queryset = Payment.objects.filter(deleted_at__isnull=True)
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAuthenticated,)
+    serializer_action_classes = {
+        "trashed": PaymentTrashedSerializer
+    }
+
+    def list(self, request):
+        return Response([])
+
+    def retrieve(self, request):
+        return Response([])
+
+    def create(self, request):
+        data = request.data
+        data["created_by"] = request.user
+        try:
+            income = Income.objects.get(pk=data["income"])
+        except Income.DoesNotExist:
+            return Response({"error": "Income does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+        return checkAndReturn(request.user, income.project, "project_payments_c",
+                              paymentCreate(self, data, income))
+
+    def update(self, request, pk=None):
+        payment = self.get_object()
+        return checkAndReturn(request.user, payment.income.project, "project_payments_u",
+                              paymentUpdate(self, request, payment))
+
+    def destroy(self, request, pk=None):
+        payment = self.get_object()
+        return checkAndReturn(request.user, payment.income.project, "project_incomes_d",
+                              delete(self, request, Income))
+
+    def get_serializer_class(self):
+        try:
+            return self.serializer_action_classes[self.action]
+        except(KeyError, AttributeError):
+            return super().get_serializer_class()
+
+    def get_queryset(self):
+        try:
+            return self.queryset_actions[self.action]
+        except (KeyError, AttributeError):
+            return super().get_queryset()
